44789e527eb01627c4f2b6345fea7ccfed779b98
Jrope21 <joshbball8@gmail.com>
2020-03-25 21:56:58 -0500

    saving da penzzz

---
+++ code-penzz/maze/maze.component.jsx
@@ -0,0 +1,21 @@
+// This component is incomplete 
+// still working out box values
+
+<div class="box">
+  <div class="box-back"> </div>
+</div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box">
+  
+</div>
+<div class="box">
+  
+</div>
+<div class="box">
+  
+</div>
+<div class="box">
+  
+</div>
